64348b213e05e5b9bf439b9d637bac0171eda3bf
YongMinSun <ddoni1010@gmail.com>
2019-11-18 19:59:08 +0900

    Initial commit

---
+++ app/src/main/java/com/tioeun/a20191118_01_getmethodpractice/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.tioeun.a20191118_01_getmethodpractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var mContext = this
+
+    abstract fun setupEvents()
+
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/tioeun/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.tioeun.a20191118_01_getmethodpractice
+
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
