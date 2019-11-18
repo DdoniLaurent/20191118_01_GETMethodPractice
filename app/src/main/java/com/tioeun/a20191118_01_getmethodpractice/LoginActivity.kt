d9a7f39ae039b656e1408c49be758ec4f51f2f33
YongMinSun <ddoni1010@gmail.com>
2019-11-18 20:14:19 +0900

    아이디 저장여부 구별

--- app/src/main/java/com/tioeun/a20191118_01_getmethodpractice/LoginActivity.kt
+++ app/src/main/java/com/tioeun/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -1,16 +1,27 @@
 package com.tioeun.a20191118_01_getmethodpractice
 
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
-
+        loginBtn.setOnClickListener {
+            if(rememberIdCheckBox.isChecked){
+                Toast.makeText(mContext, "아이디를 저장해야 합니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(mContext, "아이디를 저장 X", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun setValues() {
