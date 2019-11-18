eb673bceec96ebfe48764f5d89d5e7acdae99cfe
YongMinSun <ddoni1010@gmail.com>
2019-11-18 21:07:51 +0900

    아이디 저장여부 체크박스 => ContextUtil 활용 연습문제

--- app/src/main/java/com/tioeun/a20191118_01_getmethodpractice/utils/ContextUtil.kt
+++ app/src/main/java/com/tioeun/a20191118_01_getmethodpractice/utils/ContextUtil.kt
@@ -12,6 +12,9 @@ class ContextUtil {
 //        사용자의 아이디를 저장하는 항목 이름
         val USER_ID = "USER_ID"
 
+//        아이디 기억저장 여부
+        val ID_REMEMBER = "ID_REMEMBER"
+
 //        사용자 아이디를 저장해주는 func
         fun setUserId(context: Context, userId:String) {
 
@@ -28,6 +31,23 @@ class ContextUtil {
             var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
 
             return pref.getString(USER_ID, "")!!
+        }
+
+        fun setRemember(context: Context, isRemember:Boolean) {
+
+//          메모장(파일이름 : PracticePrefference)을 실제로 여는 동작
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+//          내용을 작성하고 SAVE 버튼을 누름.
+            pref.edit().putBoolean(ID_REMEMBER, isRemember).apply()
+        }
+
+        fun getIdRemember(context: Context) : Boolean {
+
+//          메모장(파일이름 : PracticePrefference)을 실제로 여는 동작
+            var pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+
+            return pref.getBoolean(ID_REMEMBER, false)!!
         }
     }
 }
